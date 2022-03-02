465b27b592964e0c9d61be310524507d20cf2740
Cathryn Ackerman <69750339+CatAcker@users.noreply.github.com>
2022-03-02 16:44:33 +0200

    update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/catherinaCV'));
+app.use(express.static(__dirname + '/dist/catherina-cv'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/catherinaCV/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/catherina-cv/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
